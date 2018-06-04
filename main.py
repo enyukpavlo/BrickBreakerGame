cc315f6ff9d497793046fa4dc29a19b41cdf18e4
user <enyukpavlo@gmail.com>
2018-06-04 10:25:39 -0400

    Comments added.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from kivy.clock import Clock
 class Paddle(Widget):
     score = NumericProperty(0)
 
-    def bounce_ball(self, ball):
+    def bounce_ball(self, ball):	#inverts ball vector when bouncing
         if self.collide_widget(ball):
             vx, vy = ball.velocity
             offset = (ball.center_x - self.center_y) / (self.width / 3)
@@ -30,27 +30,27 @@ class Game(Widget):
     ball = ObjectProperty(None)
     player1 = ObjectProperty(None)
 
-    def serve_ball(self, vel=(0, 10)):
+    def serve_ball(self, vel=(0, 10)):	#serving ball position and vector
         self.ball.center = self.center
         self.ball.velocity = vel
 
     def update(self, dt):
         self.ball.move()
 
-        self.player1.bounce_ball(self.ball)
+        self.player1.bounce_ball(self.ball)	#bounces ball off paddles
 
-        if (self.ball.x < self.x) or (self.ball.right > self.right):
+        if (self.ball.x < self.x) or (self.ball.right > self.right): #bounces ball off left and right
             self.ball.velocity_x *= -1
-        if (self.ball.top > self.top):
+        if (self.ball.top > self.top):	#bounces ball off top
         	self.ball.velocity_y *= -1
 
-        if (self.ball.y < self.y):
+        if (self.ball.y < self.y):	#serves ball if if it goes bellow x axist
         	self.serve_ball(vel=(0, -4))
 
 
 
 
-    def on_touch_move(self, touch):
+    def on_touch_move(self, touch):	#moves the player's paddle
         if touch.y < self.top:
             self.player1.center_x = touch.x
 
@@ -64,3 +64,8 @@ class BreakingApp(App):
 
 if __name__ == '__main__':
     BreakingApp().run()
+
+#TO DO:
+#Add destroyable bricks
+#Limit paddle moving speed
+#ball bounces off paddle strangely, fix.
